dd2f71500c81ec0269986583c79ba98f95cb39ca
Bernhard Obermoser <bernhard@MacBook-Pro-von-Bernhard.local>
2024-06-26 19:20:05 +0200

    Pickup & Interact

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -3,27 +3,47 @@ extends CharacterBody3D
 @export var normal_speed := 3.0
 @export var sprint_speed := 5.0
 @export var mouse_sensitivity := 0.005
-@export var jump_velocity := 4.0
+@export var jump_velocity := 5.0
 @export var gravity := 0.2
 
 @onready var head:Node3D = $Head
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _physics_process(delta: float) -> void:
 	move()
 	
-
 func move() -> void:
+	var is_sprinting : bool
+	if is_on_floor():
+		is_sprinting = Input.is_action_pressed("sprint")
+		if Input.is_action_just_pressed("jump"):
+			velocity.y += jump_velocity
+	else:
+		velocity.y -= gravity
+		is_sprinting = false
+	
+	var speed = normal_speed if not is_sprinting else sprint_speed
+	
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := transform.basis * Vector3(input_dir.x, 0, input_dir.y)
 	
-	velocity.z = direction.z * normal_speed
-	velocity.x = direction.x * normal_speed
+	velocity.z = direction.z * speed
+	velocity.x = direction.x * speed
+	
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		look_around(event.relative)
 		
+		
 func look_around(relative: Vector2) -> void:
 	rotate_y(-relative.x * mouse_sensitivity)
 	head.rotate_x(-relative.y * mouse_sensitivity)
+	head.rotation_degrees.x = clampf(head.rotation_degrees.x, -90, 90)
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
