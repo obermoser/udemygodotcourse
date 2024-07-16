2cf37fb5678e013af49161cf3bd0307a69215ba6
Bernhard <bernhard.obermoser@icloud.com>
2024-07-16 09:22:12 +0200

    Skip Subview Lecture

--- actors/player/player_main_camera.gd
+++ actors/player/player_main_camera.gd
@@ -5,8 +5,3 @@ extends Camera3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	equipment_cam.global_transform = global_transform
